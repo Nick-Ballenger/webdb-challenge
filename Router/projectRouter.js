ce1765e764341d3e1b43dbffe857092663b2e464
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-05-03 14:47:36 -0500

    cleaned things

--- Router/projectRouter.js
+++ Router/projectRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const Project = require('./projectModel');
+const db = require('../data/dbconfig');
 
 router.get('/', (req, res) => {
     Project.find()
@@ -14,22 +15,9 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id', async (req, res) => {
-  try {
-    const project = await Project.findById(req.params.id);
-    if (project) {
-      res.status(200).json(project);
-    } else {
-      res.status(404).json({ message: 'We could not find the project' });
-    }
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'We ran into an error retrieving the project' });
-  }
-});
 
-router.get("/project/:id", (req, res) => {
+
+router.get("/:id", (req, res) => {
   const { id } = req.params;
   db("projects")
     .where({ id: id })
